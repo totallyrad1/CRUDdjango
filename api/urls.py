25fc6a909ebb3e7a254dae51413cbdc39ff08f1d
Asaad Naji <assaadnaji014@gmail.com>
2024-11-22 15:18:42 +0100

    missing location/<int:pk> just changed it

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('item/', ItemList.as_view()),
     path('item/<int:pk>', ItemDetail.as_view()),
     path('location/', LocationList.as_view()),
-    path('item/<int:pk>', LocationDetail.as_view()),
+    path('location/<int:pk>', LocationDetail.as_view()),
 ]
